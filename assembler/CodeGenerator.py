42f26f742df4696bb1d3b53032784a47652c7111
John Wilkes <jwilkes0011@gmail.com>
2016-04-03 19:26:05 -0500

    Adding register number to symbol

--- assembler/CodeGenerator.py
+++ assembler/CodeGenerator.py
@@ -6,11 +6,28 @@ class CodeGenerator(object):
 
     def generate(self, expressions):
         with open(self.args.output_file, 'w') as f:
+            symbolCount = 0
+
             for exp in expressions:
                 # look up keyword
                 kw = syntax.KEYWORDS[exp[0]]
+
                 symbolBin = kw.opCode << 3
+                if kw.numArgs == 1:
+                    # get the register number
+                    reg = exp[1]
+                    regNum = int(reg[1])
+                    symbolBin |= regNum
+
+                # convert symbol from binary to string
                 symbolStr = '{:02X}'.format(symbolBin)
 
                 f.write(symbolStr)
                 f.write('\n')
+
+                symbolCount += 1
+
+            # fill the rest of the file with NOPs
+            while symbolCount < 256:
+                f.write('00\n')
+                symbolCount += 1
